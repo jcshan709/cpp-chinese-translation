41eb61d041cbd02437c22393a24aa4a72ad01528
sjc0910 <1826543852@qq.com>
2021-10-25 22:31:35 +0800

    第一次提交

---
+++ keywords.h
@@ -0,0 +1,27 @@
+#define 整型 int
+#define 长 long
+#define 浮点数 double
+#define 字符 char
+#define 无符号 unsigned
+#define 有符号 signed
+#define 使用 using
+#define 命名空间 namespace
+#define 如果 if
+#define 否则 else
+#define 类 class
+#define 结构体 struct
+#define 分配内存(type) new type()
+#define 分配初始化内存(type, init) new type(init)
+#define 分配数组(type, size) new type[size]()
+#define 指针 *
+#define 二级指针 **
+#define 三级指针 ***
+#define 四级指针 ****
+#define 五级指针 *****
+#define 十级指针 **********
+#define 引用 &
+#define 不变量 const
+#define 常量 constexpr
+#define 右值引用 &&
+#define 主程序 int main(int argc, char **argv)
+#define 返回 return
---
+++ stdcpp.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <exception>
+#define 字符串 std::string
+#define 标准库 std
+#define 向量 std::vector
+#define 输出 std::cout
+#define 输入 std::cin
+#define 换行 std::endl
+#define 异常 std::exception
