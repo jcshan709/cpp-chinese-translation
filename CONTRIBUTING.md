4295c522c80490af3015e79521159b63722b88b6
sjc0910 <1826543852@qq.com>
2021-10-26 22:09:54 +0800

    稍加改动

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,5 @@
 # 贡献
+欢迎为此项目贡献，你的名字可能会写在下方列表中  
 |Github账户|GitLab账户|贡献|
 |-|-|-|
 |[sjc0910](https://github.com/sjc0910)|[sjc0910/Junchao Shan](https://gitlab.com/sjc0910)|所有者&翻译贡献者|
--- src/stdc.h
+++ src/stdc.h
@@ -9,7 +9,7 @@ extern "C" {
 #define C输出 ::printf
 #define C输入 ::scanf
 #define 退出程序 ::exit
-#define 终端指令 ::system
+#define 终端命令 ::system
 #define 字节数类型 ::size_t
 
 #ifdef __cplusplus
